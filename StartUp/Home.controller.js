23973846880227445874ef7f2dd83c4527e8bc45
Keshav <nknnkumar@yahoo.com>
2023-12-08 17:53:00 +0530

    keshav

--- StartUp/Home.controller.js
+++ StartUp/Home.controller.js
@@ -1,7 +1,9 @@
 var path = require('path');
 
 let GetFunc = (req, res, next) => {
-    res.sendFile(path.join(__dirname + `/../public/Index.html`));
+    // res.sendFile(path.join(__dirname + `/../public/Index.html`));
+    console.log("aaaaaaaaaaa : ", path.join(__dirname + `/../public/Cover/index.html`));
+    res.sendFile(path.join(__dirname + `/../public/Cover/index.html`));
 };
 
 module.exports = {
