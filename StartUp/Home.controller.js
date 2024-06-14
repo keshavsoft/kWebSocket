6ca72a661e59a5cd498b5378dffb34f2c4f3a650
keshavsoft <nknnkumar@yahoo.com>
2024-06-14 15:22:17 +0530

    keshav

--- StartUp/Home.controller.js
+++ StartUp/Home.controller.js
@@ -1,4 +1,8 @@
 import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 let GetFunc = (req, res, next) => {
     console.log("aaaaaaaaaaa : ", path.join(__dirname + `/../public/Cover/index.html`));
